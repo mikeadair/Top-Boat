698926a7ead605d61846ceb9d3eed831e00fec6a
Kaleb Baldwin <kjbaldwin@bsu.edu>
2017-02-21 20:02:26 -0500

    make base application class and README

---
+++ src/cs222/topboat/TopBoatApplication.java
@@ -0,0 +1,25 @@
+package cs222.topboat;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class TopBoatApplication extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        Scene scene = new Scene(root, 800, 800);
+        primaryStage.setTitle("Top Boats");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
