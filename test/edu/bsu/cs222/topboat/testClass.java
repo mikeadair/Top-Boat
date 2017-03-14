9858ab116a919aa0ab92d0f1fc78eb926b40e3ff
ZacharyToney <zach@clearseries.com>
2017-03-13 23:41:34 -0400

    adding basic test

---
+++ test/edu/bsu/cs222/topboat/testClass.java
@@ -0,0 +1,14 @@
+package edu.bsu.cs222.topboat;
+
+import cs222.topboat.controllers.GameBoardController;
+import cs222.topboat.controllers.MainMenuController;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class testClass {
+    @Test
+    public void TestBasicTest(){
+        boolean fail = false;
+        Assert.assertFalse(fail);
+    }
+}
