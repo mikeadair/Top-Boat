fcd09876fd8307bdb0c858f0754e0fa72c551054
Michael Adair <michaeladair1996@gmail.com>
2017-03-16 01:51:01 -0400

    creates arsenal and adds to view

--- src/edu/bsu/css22/topboat/controllers/ArsenalController.java
+++ src/edu/bsu/css22/topboat/controllers/ArsenalController.java
@@ -1,17 +1,59 @@
 package edu.bsu.css22.topboat.controllers;
 
+import edu.bsu.css22.topboat.models.Weapon;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.util.Callback;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class ArsenalController implements Initializable {
-    @FXML ListView weaponList;
+    @FXML ListView<Weapon> weaponListView;
+    public List<Weapon> arsenal;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        loadWeapons();
+        ObservableList<Weapon> observableList = FXCollections.observableList(arsenal);
+        weaponListView.setItems(observableList);
 
+        weaponListView.setCellFactory(new Callback<ListView<Weapon>, ListCell<Weapon>>(){
+
+            @Override
+            public ListCell<Weapon> call(ListView<Weapon> p) {
+
+                ListCell<Weapon> cell = new ListCell<Weapon>(){
+
+                    @Override
+                    protected void updateItem(Weapon weapon, boolean bool) {
+                        super.updateItem(weapon, bool);
+                        if(weapon != null) {
+                            if(weapon.type.quantity == -1){
+                                setText(weapon.type.name + ": Infinite");
+                            }else{
+                                setText(weapon.type.name + ": " + weapon.type.quantity);
+                            }
+                        }
+                    }
+
+                };
+                return cell;
+            }
+        });
+    }
+
+    public void loadWeapons(){
+        int i = 0;
+        arsenal = new ArrayList<>();
+        for(Weapon.Type type : Weapon.Type.values()){
+            arsenal.add(i, new Weapon(type));
+        }
     }
 }
--- src/edu/bsu/css22/topboat/models/Weapon.java
+++ src/edu/bsu/css22/topboat/models/Weapon.java
@@ -1,20 +1,26 @@
 package edu.bsu.css22.topboat.models;
 
 public class Weapon {
-    public int[][] values;
-    public String name;
+    public Type type;
 
-    public Weapon(int[][] values, String name){
-        this.values = values;
-        this.name = name;
+    public Weapon(Type type){
+        this.type = type;
     }
 
     public enum Type {
-        SINGLESHOT(new Weapon(new int[][]{{0,0}}, "Single Shot")),
-        CROSSSHOT(new Weapon(new int[][]{{0,0},{0,1},{0,-1},{1,0},{-1,0}}, "Cross Shot"));
+        SINGLESHOT(new int[][]{{0,0}},"Single Shot",-1),
+        CROSS_SHOT(new int[][]{{0,0},{0,1},{0,-1},{1,0},{-1,0}},"Cross Shot",0),
+        H_LINE_SHOT(new int[][]{{0,0},{0,1},{0,2},{0,-1},{0,-2}}, "H Line Shot",0),
+        V_LINE_SHOT(new int[][]{{0,0},{1,0},{1,0},{-1,0},{-2,0}}, "V Line Shot",0);
 
-        Type(Weapon aoe) { this.affectedTiles = aoe; }
+        Type(int[][] affectedTiles, String name, int quantity){
+            this.affectedTiles = affectedTiles;
+            this.name = name;
+            this.quantity = quantity;
+        }
 
-        public Weapon affectedTiles;
+        public int[][] affectedTiles;
+        public String name;
+        public int quantity;
     }
 }
