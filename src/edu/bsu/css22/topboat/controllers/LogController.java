3a3c8430f22cc333e27485596467944cd7621d6b
Michael Adair <michaeladair1996@gmail.com>
2017-03-15 23:47:41 -0400

    adds log notifications and sounds

--- src/edu/bsu/css22/topboat/controllers/LogController.java
+++ src/edu/bsu/css22/topboat/controllers/LogController.java
@@ -1,16 +1,20 @@
 package edu.bsu.css22.topboat.controllers;
 
 import javafx.application.Platform;
+import javafx.beans.property.ReadOnlyObjectProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.*;
 import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.text.Text;
 import edu.bsu.css22.topboat.models.Log;
 import edu.bsu.css22.topboat.models.Log.Message;
 
+import java.applet.Applet;
+import java.applet.AudioClip;
+import java.awt.*;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -20,8 +24,14 @@ public class LogController implements Initializable{
     @FXML ListView loglist_messages;
     @FXML Button sendButton;
     @FXML TextField messageField;
+    @FXML TabPane logs;
+    @FXML Tab game_tab;
+    @FXML Tab messages_tab;
+
     Log gameLog;
     Log chatLog;
+    public static final AudioClip game_message = Applet.newAudioClip(LogController.class.getResource("../sounds/game_message.wav"));
+    public static final AudioClip player_message = Applet.newAudioClip(LogController.class.getResource("../sounds/player_message.wav"));
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -35,6 +45,9 @@ public class LogController implements Initializable{
         gameLog.addMessageReceivedListener(newMessage -> showMessageInLog(loglist_game, newMessage));
         chatLog.addMessageReceivedListener(newMessage -> showMessageInLog(loglist_messages, newMessage));
         sendButton.setOnAction(event -> sendPlayerMessage());
+        logs.getSelectionModel().selectedItemProperty().addListener((observable,oldSelected,newSelected)->{
+            removeTabNotify(newSelected);
+        });
         messageField.setOnKeyPressed(keyEvent -> {
             if (keyEvent.getCode() == KeyCode.ENTER)  {
                 sendPlayerMessage();
@@ -47,6 +60,14 @@ public class LogController implements Initializable{
         messageField.setText("");
     }
 
+    private void removeTabNotify(Tab tab){
+        tab.setStyle(null);
+    }
+
+    private void notifyTab(Tab tab){
+        tab.setStyle("-fx-background-color: #71DE9B;");
+    }
+
     private void showMessageInLog(ListView log, Message newMessage) {
         Text messageView = new Text(newMessage.getContents());
         messageView.setFill(newMessage.getColor());
@@ -55,6 +76,18 @@ public class LogController implements Initializable{
             log.getItems().add(messageView);
         } else {
             Platform.runLater(() -> log.getItems().add(messageView));
+        }
+        log.scrollTo(log.getItems().toArray().length);
+        ReadOnlyObjectProperty<Tab> currentTab = logs.getSelectionModel().selectedItemProperty();
+        if(log == loglist_game && currentTab.getValue() != game_tab) {
+            System.out.println("GAME LOG");
+            game_message.play();
+            notifyTab(game_tab);
+        }
+        if(log == loglist_messages && currentTab.getValue() != messages_tab){
+            System.out.println("MESSAGES LOG");
+            player_message.play();
+            notifyTab(messages_tab);
         }
     }
 
