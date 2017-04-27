6330ba06af68631f25a93b82647c6c1bdf77b500
Kaleb Baldwin <kjbaldwin@bsu.edu>
2017-04-27 00:02:37 -0400

    run socket loop in socket connections that are constructed with a socket

--- src/edu/bsu/css22/topboat/ConnectMultiplayerGame.java
+++ src/edu/bsu/css22/topboat/ConnectMultiplayerGame.java
@@ -6,8 +6,11 @@ import edu.bsu.css22.topboat.models.Log;
 import javafx.application.Platform;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.net.SocketException;
 
 public class ConnectMultiplayerGame extends Game {
